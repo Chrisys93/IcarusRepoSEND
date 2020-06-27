56a2020a4be058627f8113f0af3aa0a6efdb56dc
Chrisys93 <nicolaescu3@yahoo.com>
2020-06-27 16:44:46 +0100

    Changed writeTree

--- examples/repos-mgmt/plotresults.py
+++ examples/repos-mgmt/plotresults.py
@@ -704,7 +704,7 @@ def printTree(tree, d=0):
 
 def writeTree(tree, f, d=0):
     if (tree == None or len(tree) == 0):
-        print "\t" * d, "-"
+        f.write("\t" * d, "-")
     else:
         for key, val in tree.items():
             if (isinstance(val, dict)):
